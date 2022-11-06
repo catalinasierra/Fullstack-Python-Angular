4b67e90ff0224be090e3ea373bcb7a69634670d5
LAURA MALAGON <laura.malagon@mail.escuelaing.edu.co>
2022-11-06 16:25:41 -0500

    ejercicio30

---
+++ JavaScript/Esenciales/S08 -Git/Ejercicio30.js
@@ -0,0 +1,10 @@
+document.get
+        const primerparrafo = document.getElementById("Primerparrafo");
+            primerparrafo.style.color = "red"
+        const segundoparrafo = document.getElementById("Segundoparrafo");
+        segundoparrafo.style.color = "green"
+        const tercerparrafo = document.getElementById("Tercerparrafo");
+        tercerparrafo.style.color = "blue"
+        const cuartoparrafo = document.getElementById("Cuartoparrafo");
+        cuartoparrafo.style.color = "yellow"
+        
